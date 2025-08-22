cf19ee1e74efbe09b847448fad493e05a0911924
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-22 12:27:01 +0800

    Override toString method in ChiikawaException.java

--- src/main/java/ChiikawaException.java
+++ src/main/java/ChiikawaException.java
@@ -2,4 +2,9 @@ public class ChiikawaException extends Exception {
     public ChiikawaException(String message) {
         super(message);
     }
+
+    @Override
+    public String toString() {
+        return super.getMessage();
+    }
 }
