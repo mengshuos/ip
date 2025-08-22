13215df169b752fc1a96136b1c48281a10cd93bc
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-22 10:55:32 +0800

    Add new fields to DeadlineTask and EventTask that represents the dates/times

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -2,7 +2,10 @@
  * Class for Tasks with deadlines, due before a specific date/time.
  */
 public class DeadlineTask extends Task {
+    private String deadline;
+
     public DeadlineTask(String name, String deadline) {
         super(name);
+        this.deadline = deadline;
     }
 }
--- src/main/java/EventTask.java
+++ src/main/java/EventTask.java
@@ -2,7 +2,12 @@
  * Class for Events which are tasks that start at a specific date/time and ends at specific date/time.
  */
 public class EventTask extends Task {
-    public EventTask(String name, String start, String end) {
+    private String startTime;
+    private String endTime;
+
+    public EventTask(String name, String startTime, String endTime) {
         super(name);
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 }
