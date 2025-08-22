ed48518600eda15cea5d16eeef6d8f57684a43d6
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-22 12:27:24 +0800

    Add exception handling to Chiikawa

--- src/main/java/Chiikawa.java
+++ src/main/java/Chiikawa.java
@@ -41,71 +41,88 @@ public class Chiikawa {
         System.out.println("__________________________________________________________________");
 
         while (true) {
-            nextLine = scanner.nextLine();
-            action = nextLine.split(" ", 2);
+            try {
+                nextLine = scanner.nextLine();
+                action = nextLine.split(" ", 2);
 
-            if (Objects.equals(nextLine, "bye")) {
-                System.out.println("bye~ bye~!! soon... see soon? ₍ᐢ.  ̫.ᐢ₎");
-                System.out.println("__________________________________________________________________");
-                scanner.close();
-                return;
+                if (Objects.equals(nextLine, "bye")) {
+                    System.out.println("bye~ bye~!! soon... see soon? ₍ᐢ.  ̫.ᐢ₎");
+                    System.out.println("__________________________________________________________________");
+                    scanner.close();
+                    return;
 
-            } else if (Objects.equals(nextLine, "list")) {
-                System.out.println("i- i twhink these you tasks...?");
-                for (int index = 0; index < taskListItemCount; index++) {
-                    Task currTask = taskList[index];
-                    System.out.println((index + 1) + "." + currTask.toString());
+                } else if (Objects.equals(nextLine, "list")) {
+                    System.out.println("i- i twhink these you tasks...?");
+                    for (int index = 0; index < taskListItemCount; index++) {
+                        Task currTask = taskList[index];
+                        System.out.println((index + 1) + "." + currTask.toString());
+                    }
+                    System.out.println("__________________________________________________________________");
+
+                } else if (Objects.equals(action[0], "mark")) {
+                    int index = Integer.parseInt(action[1]);
+                    taskList[index - 1].markTask();
+                    System.out.println("okee! you finis~~");
+                    System.out.println(taskList[index - 1].toString());
+                    System.out.println("__________________________________________________________________");
+
+                } else if (Objects.equals(action[0], "unmark")) {
+                    int index = Integer.parseInt(action[1]);
+                    taskList[index - 1].unmarkTask();
+                    System.out.println("ohh.... slowpoke!!");
+                    System.out.println(taskList[index - 1].toString());
+                    System.out.println("__________________________________________________________________");
+
+                } else if (Objects.equals(action[0], "todo")) {
+                    if (action.length != 2) {
+                        throw new ChiikawaException("no name!! u giv no name!!");
+                    }
+
+                    System.out.println("wahhh! oke, me add add:");
+                    taskList[taskListItemCount] = new ToDoTask(action[1]);
+                    System.out.println("  " + taskList[taskListItemCount].toString());
+                    taskListItemCount++;
+                    System.out.println("wuuu! " + Task.getTaskCount() + " tasks in list now!");
+                    System.out.println("__________________________________________________________________");
+
+                } else if (Objects.equals(action[0], "deadline")) {
+                    specifiedTime = action[1].split("/");
+
+                    if (specifiedTime.length != 3) {
+                        throw new ChiikawaException("wher...where is time!!! giv me gud deadline!!!");
+                    }
+
+                    System.out.println("ohh! de..deadline?! uuuhh, me add me add:");
+                    taskList[taskListItemCount] = new DeadlineTask(specifiedTime[0], specifiedTime[1]);
+                    System.out.println("  " + taskList[taskListItemCount].toString());
+                    taskListItemCount++;
+                    System.out.println("wuuu! " + Task.getTaskCount() + " tasks in list now!");
+                    System.out.println("__________________________________________________________________");
+
+                } else if (Objects.equals(action[0], "event")) {
+                    specifiedTime = action[1].split("/");
+
+                    if (specifiedTime.length != 3) {
+                        throw new ChiikawaException("wher...where is time!!! giv me 2!! 2 time!!");
+                    }
+
+                    System.out.println("wowzies! yayy! can me go? me add me add:");
+                    taskList[taskListItemCount] = new EventTask(specifiedTime[0], specifiedTime[1], specifiedTime[2]);
+                    System.out.println("  " + taskList[taskListItemCount].toString());
+                    taskListItemCount++;
+                    System.out.println("wuuu! " + Task.getTaskCount() + " tasks in list now!");
+                    System.out.println("__________________________________________________________________");
+
+                } else {
+                    throw new ChiikawaException("wha...wha? i dun kno... scawy...");
+
                 }
-                System.out.println("__________________________________________________________________");
 
-            } else if (Objects.equals(action[0], "mark")) {
-                int index = Integer.parseInt(action[1]);
-                taskList[index - 1].markTask();
-                System.out.println("okee! you finis~~");
-                System.out.println(taskList[index - 1].toString());
-                System.out.println("__________________________________________________________________");
-
-            } else if (Objects.equals(action[0], "unmark")) {
-                int index = Integer.parseInt(action[1]);
-                taskList[index - 1].unmarkTask();
-                System.out.println("ohh.... slowpoke!!");
-                System.out.println(taskList[index - 1].toString());
-                System.out.println("__________________________________________________________________");
-
-            } else if (Objects.equals(action[0], "todo")) {
-                System.out.println("wahhh! oke, me add add:");
-                taskList[taskListItemCount] = new ToDoTask(action[1]);
-                System.out.println("  " + taskList[taskListItemCount].toString());
-                taskListItemCount++;
-                System.out.println("wuuu! " + Task.getTaskCount() + " tasks in list now!");
-                System.out.println("__________________________________________________________________");
-
-            } else if (Objects.equals(action[0], "deadline")) {
-                specifiedTime = action[1].split("/");
-                System.out.println("ohh! de..deadline?! uuuhh, me add me add:");
-                taskList[taskListItemCount] = new DeadlineTask(specifiedTime[0], specifiedTime[1]);
-                System.out.println("  " + taskList[taskListItemCount].toString());
-                taskListItemCount++;
-                System.out.println("wuuu! " + Task.getTaskCount() + " tasks in list now!");
-                System.out.println("__________________________________________________________________");
-
-            } else if (Objects.equals(action[0], "event")) {
-                specifiedTime = action[1].split("/");
-                System.out.println("wowzies! yayy! can me go? me add me add:");
-                taskList[taskListItemCount] = new EventTask(specifiedTime[0], specifiedTime[1], specifiedTime[2]);
-                System.out.println("  " + taskList[taskListItemCount].toString());
-                taskListItemCount++;
-                System.out.println("wuuu! " + Task.getTaskCount() + " tasks in list now!");
-                System.out.println("__________________________________________________________________");
-
-            } else {
-                taskList[taskListItemCount] = new Task(nextLine);
-                taskListItemCount++;
-                System.out.println("added!!: " + nextLine);
+            } catch (ChiikawaException e) {
+                System.out.println(e.toString());
                 System.out.println("__________________________________________________________________");
 
             }
         }
-
     }
 }
