c5641005e5147d5e6c147ee3110f668b628a7cb7
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-29 18:13:03 +0800

    feat: Add checks for valid file and directory in Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,13 +1,36 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Storage {
     private String filePath;
     private File f;
+    private File parentDir;
 
     public Storage(String filePath) {
         this.filePath = filePath;
         this.f = new File(filePath);
+        this.parentDir = f.getParentFile();
+
+        // Checking if parent directory exists.
+        if (!parentDir.exists()) {
+            // Call UI function to print out error.
+            // Placeholder print statements.
+            System.out.println("diwectory nawt faound!!");
+            System.out.println("i make, no worry");
+            parentDir.mkdirs();
+        }
+
+        // Checking if the file exists.
+        try {
+            if (!f.exists()) {
+                System.out.println("file nawt faound!!");
+                System.out.println("i make, no worry");
+                f.createNewFile();
+            }
+        } catch (IOException e) {
+            // Placeholder for UI function to print.
+        }
     }
 
     public ArrayList<Task> loadFile() {
