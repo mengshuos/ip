ba46c7e0a733dc1d752d0cf20823ff4df970976b
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-29 18:21:32 +0800

    feat: Add basic logic for loadFile function in Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Storage {
     private String filePath;
@@ -34,7 +35,50 @@ public class Storage {
     }
 
     public ArrayList<Task> loadFile() {
-        return null;
+        Scanner s = null;
+        ArrayList<Task> taskList = new ArrayList<>();
+
+        try {
+            s = new Scanner(f);
+        } catch (IOException e) {
+            // Placeholder for UI function.
+            System.out.println("something wrong!!");
+        }
+
+        while(s.hasNextLine()) {
+            // Placeholder for parser that would return a String[] containing information we need.
+            String[] line = s.nextLine().split("\\|", 4);
+            boolean isCompleted = line[1].strip().equals("1");
+            switch (line[0].strip()) {
+                case "T":
+                    // Create ToDoTask.
+                    // Placeholder for TaskList function that adds new Task.
+                    ToDoTask newToDoTask = new ToDoTask(line[2].strip(), isCompleted);
+                    taskList.add(newToDoTask);
+                    break;
+
+                case "D":
+                    // Create DeadlineTask.
+                    // Placeholder for TaskList function that adds new Task.
+                    DeadlineTask newDeadlineTask = new DeadlineTask(line[2].strip(),
+                            isCompleted,
+                            line[3].strip());
+                    taskList.add(newDeadlineTask);
+                    break;
+
+                case "E":
+                    // Create EventTask.
+                    // Placeholder for TaskList function that adds new Task.
+                    String[] eventTime = line[3].split("to");
+                    EventTask newEventTask = new EventTask(line[2].strip(),
+                            isCompleted,
+                            eventTime[0].strip(),
+                            eventTime[1].strip());
+                    taskList.add(newEventTask);
+                    break;
+            }
+        }
+        return taskList;
     }
 
     public void saveFile(ArrayList<Task> taskList) {
