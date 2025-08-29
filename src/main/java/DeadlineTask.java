9406f39c72fe07731921bcf0739568fec9e62228
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-29 16:51:55 +0800

    refactor: Abstract out reformatTime function into parent Task class

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,7 +1,4 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Objects;
 
 /**
  * Class for Tasks with deadlines, due before a specific date/time.
@@ -9,38 +6,22 @@ import java.util.regex.Pattern;
 public class DeadlineTask extends Task {
     private String deadline;
     private String formattedDeadline;
-    private Pattern pattern = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
-    private Matcher matcher;
 
     public DeadlineTask(String name, String deadline) {
         super(name);
         this.deadline = deadline;
-        this.formattedDeadline = reformatTime(deadline);
+        this.formattedDeadline = super.reformatTime(deadline);
     }
 
     public DeadlineTask(String name, boolean isCompleted, String deadline) {
         super(name, isCompleted);
         this.deadline = deadline;
-        this.formattedDeadline = reformatTime(deadline);
-    }
-
-    private String reformatTime(String time) {
-        matcher = pattern.matcher(time);
-        LocalDate localDateTime = null;
-        String formattedDate = "";
-
-        if (matcher.find()) {
-            deadline = matcher.group();
-            localDateTime = LocalDate.parse(time);
-            formattedDate = localDateTime.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-        }
-        
-        return formattedDate;
+        this.formattedDeadline = super.reformatTime(deadline);
     }
 
     @Override
     public String toString() {
-        String outputTime = this.formattedDeadline == "" ? this.deadline : this.formattedDeadline;
+        String outputTime = Objects.equals(this.formattedDeadline, "") ? this.deadline : this.formattedDeadline;
         return "D " + super.toString() + " | " + outputTime;
     }
 }
