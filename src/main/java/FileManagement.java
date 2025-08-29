aa74465a6e26c434d6532d1cf8a6a84ddf4f6673
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-29 11:43:49 +0800

    feat: Add ability to load in files from hard disk

--- src/main/java/FileManagement.java
+++ src/main/java/FileManagement.java
@@ -7,24 +7,58 @@ import java.util.Scanner;
 
 public class FileManagement {
     private String filePath;
-    private File f;
-    private FileWriter fw;
 
     public FileManagement(String filePath) {
         this.filePath = filePath;
     }
 
     public ArrayList<Task> loadFile() {
-        try (Scanner scanner = new Scanner(new File(filePath))) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
+        File f = new File(this.filePath);
+        Scanner s = null;
 
+        try {
+            if (!f.exists()) {
+                System.out.println("file nawt faound!!");
+                System.out.println("i make, no worry");
+                f.createNewFile();
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("cwan't start!! no can find file!!");
+            s = new Scanner(f);
+        } catch (IOException e) {
+            System.out.println("something wrong!!");
         }
 
-        return null;
+        ArrayList<Task> taskList = new ArrayList<>();
+        while(s.hasNextLine()) {
+            String[] line = s.nextLine().split("\\|", 4);
+            boolean isCompleted = line[1].strip().equals("1");
+            switch (line[0]) {
+                case "T":
+                    // Create ToDoTask.
+                    ToDoTask newToDoTask = new ToDoTask(line[2].strip(), isCompleted);
+                    taskList.add(newToDoTask);
+                    break;
+
+                case "D":
+                    // Create DeadlineTask.
+                    DeadlineTask newDeadlineTask = new DeadlineTask(line[2].strip(),
+                            isCompleted,
+                            line[3].strip());
+                    taskList.add(newDeadlineTask);
+                    break;
+
+                case "E":
+                    // Create EventTask.
+                    String[] eventTime = line[3].split("to");
+                    EventTask newEventTask = new EventTask(line[2].strip(),
+                            isCompleted,
+                            eventTime[0].strip(),
+                            eventTime[1].strip());
+                    taskList.add(newEventTask);
+                    break;
+            }
+        }
+
+        return taskList;
     }
 
     public void saveFile(ArrayList<Task> taskList) {
@@ -37,13 +71,8 @@ public class FileManagement {
             fw.close();
 
         } catch (IOException e) {
+            System.out.println(e);
             System.out.println("file got pwoblem!");
         }
-    }
-
-    public void writeToFile(String textToWrite) throws IOException {
-        fw = new FileWriter(filePath);
-        this.fw.write(textToWrite);
-        this.fw.close();
     }
 }
