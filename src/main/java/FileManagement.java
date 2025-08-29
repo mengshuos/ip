d122e09f5d639c7d50378df4d208cf1a6a2fd0b2
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-29 10:50:50 +0800

    feat: Add ability to save files into the hard disk

--- src/main/java/FileManagement.java
+++ src/main/java/FileManagement.java
@@ -2,31 +2,48 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileManagement {
+    private String filePath;
     private File f;
-    private Scanner s;
     private FileWriter fw;
 
     public FileManagement(String filePath) {
-        this.f = new File(filePath);
+        this.filePath = filePath;
+    }
+
+    public ArrayList<Task> loadFile() {
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("cwan't start!! no can find file!!");
+        }
+
+        return null;
+    }
+
+    public void saveFile(ArrayList<Task> taskList) {
         try {
-            this.fw = new FileWriter(this.f, true);
-            this.s = new Scanner(this.f);
+            FileWriter fw = new FileWriter(this.filePath, false);
+            for (int i = 0; i < taskList.size(); i++) {
+                Task currTask = taskList.get(i);
+                fw.append(currTask.toString());
+            }
+            fw.close();
+
         } catch (IOException e) {
-            System.out.println("wha wha is IOException?");
+            System.out.println("file got pwoblem!");
         }
     }
 
-    public void printFileContents() throws FileNotFoundException {
-        while (this.s.hasNext()) {
-            System.out.println(this.s.nextLine());
-        }
-    }
-
-    public void appendToFile(String textToAppend) throws IOException {
-        this.fw.append(textToAppend);
+    public void writeToFile(String textToWrite) throws IOException {
+        fw = new FileWriter(filePath);
+        this.fw.write(textToWrite);
         this.fw.close();
     }
 }
