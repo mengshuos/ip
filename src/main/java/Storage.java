1a1819e9c487df84d663265aeb69e1a539e484ad
mengshuos <41373900+mengshuos@users.noreply.github.com>
2025-08-30 15:14:36 +0800

    refactor: Change print statements into ChiikawaExceptions

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -13,54 +13,43 @@ public class Storage {
         this.filePath = filePath;
         this.f = new File(filePath);
         this.parentDir = f.getParentFile();
-
-        // Checking if parent directory exists.
-        if (!parentDir.exists()) {
-            // THROW ERROR THAT HAS THE FOLLOWING PRINTS
-            System.out.println("diwectory nawt faound!!");
-            System.out.println("i make, no worry");
-            parentDir.mkdirs();
-        }
-
-        // Checking if the file exists.
-        try {
-            if (!f.exists()) {
-                // THROW ERROR THAT HAS THE FOLLOWING PRINTS
-                System.out.println("file nawt faound!!");
-                System.out.println("i make, no worry");
-                f.createNewFile();
-            }
-        } catch (IOException e) {
-            // NEED FIX TMR
-        }
     }
 
-    public ArrayList<Task> loadFile() {
+    public ArrayList<Task> loadFile() throws ChiikawaException {
         Scanner s = null;
         ArrayList<Task> taskList = new ArrayList<>();
+
+        if (!this.parentDir.exists()) {
+            this.parentDir.mkdirs();
+            throw new ChiikawaException("diwectory nawt faound!!\ni make, no worry")
+        }
+
+        if (!this.f.exists()) {
+            try {
+                this.f.createNewFile();
+                throw new ChiikawaException("file nawt faound!!\ni make, no worry");
+            } catch (IOException e) {
+                throw new ChiikawaException("IOException or samting... idkk!!!");
+            }
+        }
 
         try {
             s = new Scanner(f);
         } catch (IOException e) {
-            // Placeholder for UI function.
-            System.out.println("something wrong!!");
+            throw new ChiikawaException("scanner pwoblem....");
         }
 
         while(s.hasNextLine()) {
-            // Placeholder for parser that would return a String[] containing information we need.
             String[] line = Parser.parseTaskInfo(s.nextLine(), "\\|", 4);
             boolean isCompleted = line[1].equals("1");
-            switch (line[0]) {
+            String command = line[0];
+            switch (command) {
                 case "T":
-                    // Create ToDoTask.
-                    // Placeholder for TaskList function that adds new Task.
                     ToDoTask newToDoTask = new ToDoTask(line[2], isCompleted);
                     taskList.add(newToDoTask);
                     break;
 
                 case "D":
-                    // Create DeadlineTask.
-                    // Placeholder for TaskList function that adds new Task.
                     DeadlineTask newDeadlineTask = new DeadlineTask(line[2],
                             isCompleted,
                             line[3]);
@@ -68,8 +57,6 @@ public class Storage {
                     break;
 
                 case "E":
-                    // Create EventTask.
-                    // Placeholder for TaskList function that adds new Task.
                     String[] eventTime = Parser.parseTaskInfo(line[3], "to", 2);
                     EventTask newEventTask = new EventTask(
                             line[2],
@@ -83,20 +70,16 @@ public class Storage {
         return taskList;
     }
 
-    public void saveFile(ArrayList<Task> taskList) {
+    public void saveFile(ArrayList<Task> taskList) throws ChiikawaException {
         try {
             FileWriter fw = new FileWriter(this.filePath, false);
-            for (int i = 0; i < taskList.size(); i++) {
-                Task currTask = taskList.get(i);
-                fw.append(currTask.toString() + "\n");
+            for (Task currTask : taskList) {
+                fw.append(currTask.toString()).append("\n");
             }
             fw.close();
 
         } catch (IOException e) {
-            // THROW ERROR THAT DOES THE FOLLOWING
-            // Placeholder for UI function.
-            System.out.println(e);
-            System.out.println("file got pwoblem!");
+            throw new ChiikawaException("file got pwoblem!!");
         }
     }
 }
